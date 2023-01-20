9bec3e2d5316ada1717b60504b18034214505093
Jeeva <70736311+jeeva3006@users.noreply.github.com>
2023-01-20 13:17:28 +0530

    Update database.ts

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,7 +1,7 @@
 export const databaseCredentials = {
     host: '194.163.40.154',
-    user: 'jee',
-    password: 'awdsawds',
+    user: 'jee6',
+    password: 'awdsawdS@jee6.in',
     port: 3306,
     database: 'youtube',
     connectionLimit: 10,
